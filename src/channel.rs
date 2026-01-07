e630091ff14c3d64696a9f6b7543c93355b784c2
Pierce Bartine <piercebartine@gmail.com>
2026-01-06 17:25:58 -0800

    chore: Add methods

--- src/channel.rs
+++ src/channel.rs
@@ -15,6 +15,7 @@ use ssh2::ReadWindow;
 use ssh2::WriteWindow;
 
 use crate::RuntimeContext;
+use crate::session;
 
 /// Async wrapper for [`ssh2::Channel`].
 pub struct Channel<C: RuntimeContext> {
@@ -23,9 +24,64 @@ pub struct Channel<C: RuntimeContext> {
     ctx: C,
 }
 
+/// Constructors and non-wrapping functions
+impl<C: RuntimeContext> Channel<C> {
+    pub(crate) const fn new(inner: ssh2::Channel, session: ssh2::Session, ctx: C) -> Self {
+        Self {
+            inner,
+            session,
+            ctx,
+        }
+    }
+}
+
+/// Async wrappers
+impl<C: RuntimeContext> Channel<C> {
+    // setenv
+    // request_pty
+    // request_pty_size
+    // request_auth_agent_forwarding
+    // exec
+    // shell
+    // subsystem
+    // process_startup
+    // handle_extended_data
+    // adjust_receive_window
+    // send_eof
+    // wait_eof
+    // close
+    // wait_close
+}
+
+/// Sync wrappers
+impl<C: RuntimeContext> Channel<C> {
+    // stderr
+    // stream
+    // exit_status
+    // exit_signal
+    // read_window
+    // write_window
+    // eof
+}
+
+// TODO: AsyncRead + AsyncWrite
+
 /// Async wrapper for [`ssh2::Stream`].
 pub struct Stream<C: RuntimeContext> {
     inner: ssh2::Stream,
     session: ssh2::Session,
     ctx: C,
 }
+
+/// Constructors and non-wrapping functions
+impl<C: RuntimeContext> Stream<C> {
+    pub(crate) const fn new(inner: ssh2::Stream, session: ssh2::Session, ctx: C) -> Self {
+        Self {
+            inner,
+            session,
+            ctx,
+        }
+    }
+}
+
+// TODO: AsyncRead + AsyncWrite
--- src/session.rs
+++ src/session.rs
@@ -18,3 +18,74 @@ pub struct Session<C: RuntimeContext> {
     inner: ssh2::Session,
     ctx: C,
 }
+
+/// Constructors and non-wrapping functions
+impl<C: RuntimeContext> Session<C> {
+    /// Wraps an [`ssh2::Session`] with async bindings.
+    ///
+    /// # Errors
+    ///
+    /// Returns an error if the runtime context cannot be created from blocking
+    /// session. This can happen if the transport has not yet been set with
+    /// [`ssh2::Session::set_tcp_stream`].
+    pub fn from_blocking(inner: ssh2::Session) -> Result<Self, Error> {
+        inner.set_blocking(false);
+        let ctx = C::new(&inner)?;
+        Ok(Self { inner, ctx })
+    }
+
+    /// Creates an async session from the given transport stream.
+    ///
+    /// # Errors
+    ///
+    /// Returns an error if the underlying `libssh2` library has not been
+    /// initialized (unlikely, as [`ssh2`] does this automatically).
+    #[cfg(unix)]
+    pub fn from_stream(stream: impl std::os::fd::AsRawFd + 'static) -> Result<Self, Error> {
+        let mut inner = ssh2::Session::new()?;
+        inner.set_tcp_stream(stream);
+        Self::from_blocking(inner)
+    }
+}
+
+/// Async wrappers
+impl<C: RuntimeContext> Session<C> {
+    // handshake
+    // userauth_password
+    // userauth_pubkey_file
+    // userauth_pubkey_memory
+    // userauth_hostbased_file
+    // userauth_keyboard_interactive
+    // userauth_agent
+    // auth_methods
+    // method_pref
+    // supported_algs
+    // channel_session
+    // channel_direct_tcpip
+    // channel_direct_streamlocal
+    // channel_forward_listen
+    // channel_open
+    // scp_recv
+    // scp_send
+    // sftp
+    // keepalive_send
+    // disconnect
+}
+
+/// Sync wrappers
+impl<C: RuntimeContext> Session<C> {
+    // agent
+    // known_hosts
+    // host_key
+    // host_key_hash
+    // authenticated
+    // is_blocking
+    // banner
+    // banner_bytes
+    // timeout
+    // methods
+    // block_directions
+    // set_keepalive
+    // trace
+    // userauth_banner
+}
